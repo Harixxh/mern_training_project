2f1de4369dac05857954c6c25ead14aae720eaaa
Harixxh <harishmkr88@gmail.com>
2025-10-10 15:54:28 +0530

    last

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let a=6;
+let b=a;
+a=4;
+console.log(b);
+
+let obj1={
+    age:20
+};
+let obj2=obj1;
+obj1.age=25;
+console.log(obj2.age);
+
+console.log(9|5);
+console.log(9&5);
+console.log(9^5);
+
+function sum(a,b){
+    return a+b;
+}
+
+console.log(sum(5,10));
+
